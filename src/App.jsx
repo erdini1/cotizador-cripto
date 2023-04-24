d0b7fe2bfde0a59b5352c77970d637d61427c722
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-23 22:25:51 -0300

    Creando el primer Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+//Creo el primer Styled Component
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+`
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Desde APP</h1>
+      <Heading>Desde App</Heading>
     </>
   )
 }
