219d96802ec751a0cbaf83457d02e14cd51cb162
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-24 21:45:31 -0300

    Agregando estilos al precio

--- src/Components/Resultado.jsx
+++ src/Components/Resultado.jsx
@@ -1,29 +1,59 @@
 import styled from "@emotion/styled"
 
-const Parrafo = styled.p`
-    
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: "Lato", sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
 `
 
-const Resultado = ({resultado}) => {
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    span{
+        font-weight: 700;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 24px;
+    span{
+        font-weight: 700;
+    }
+`
+
+
+const Resultado = ({ resultado }) => {
 
     const {
-        PRICE, 
-        HIGHDAY, 
-        LOWDAY, 
-        CHANGEPCTDAY, 
-        IMAGEURL, 
+        PRICE,
+        HIGHDAY,
+        LOWDAY,
+        CHANGEPCTDAY,
+        IMAGEURL,
         LASTUPDATE
     } = resultado
 
     return (
-        <div>
-            <p>El precio es de: <span>{PRICE}</span></p>
-            <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-            <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-            <p>Variación últimas 24 horas: <span>{CHANGEPCTDAY}</span></p>
-            <p>Ultima Actualización: <span>{LASTUPDATE}</span></p>
-
-        </div>
+        <Contenedor>
+            <Imagen
+                src={`https://cryptocompare.com/${IMAGEURL}`}
+                alt="imgen Cripto"
+            />
+            <div>
+                <Precio>El precio es de: <span>{PRICE}</span></Precio>
+                <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+                <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+                <Texto>Variación últimas 24 horas: <span>{CHANGEPCTDAY}%</span></Texto>
+                <Texto>Ultima Actualización: <span>{LASTUPDATE}</span></Texto>
+            </div>
+        </Contenedor>
     )
 }
 
